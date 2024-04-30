7a7852f02f19c58f8b16883d3f26d35b1af9e60e
hualigushi <hualigushi@163.com>
2024-04-30 21:47:33 +0800

    feat: 备案号

--- src/App.vue
+++ src/App.vue
@@ -9,4 +9,17 @@
     <router-link to="/protocol">协议</router-link>
   </p>
   <router-view />
+  <footer>浙ICP备2024090465号-1</footer>
 </template>
+<style scoped>
+footer {
+    position: fixed;
+    bottom: 0;
+    left:0;
+    right:0;
+    color:#bbb;
+    font-size: 12px;
+    height: 48px;
+    text-align: center;
+}
+</style>
